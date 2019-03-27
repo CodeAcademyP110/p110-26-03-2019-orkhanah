2bb51151b86e93d9f516fa7ca71883c9c1bedfc9
orkhanah <46805812+orkhanah@users.noreply.github.com>
2019-03-27 10:23:50 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+"use.strict";
+
+
+// BIRINCI TAPSHIRIG
+
+// let b = []
+
+// let a = prompt("Array'in uzunlugunu teyin edin")
+
+// for (let i = 0; i < a ;i++);
+// {
+//     for (let i = 0; i < a; i++) {
+    
+//       let userInput = prompt(i+1 + "-ci Reqemi daxil edin: ")  ;
+//       let userNumber = +userInput;
+//       b.push(userNumber);
+//     }
+// }
+  
+// let max = min = b[0];
+// for (let i = 0; i < b.length; i++) {
+  
+//     if (b[i] > max) {
+//         max = b[i]
+//     }
+
+//     if (b[i] < min) {
+//         min = b[i]
+//     }
+
+// }
+
+// console.log(max , min);
+
+
+
+// // IKINCI TAPSHIRIG
+
+// let b = []
+
+// let a = prompt("Array'in uzunlugunu teyin edin")
+
+// for (let i = 0; i < a ;i++);
+// {
+//     for (let i = 0; i < a; i++) {
+    
+//       let userInput = prompt(i+1 + "-ci Reqemi daxil edin: ")  ;
+//       let userNumber = +userInput;
+//       b.push(userNumber);
+//     }
+// }
+
+// let = b[0]
+// let cutdur = []
+
+// for (let i = 0; i < b.length; i++) {
+
+//     if (b[i]%2==0) {
+//         cutdur[i] = b[i]
+//     }
+    
+// }
+
+// console.log(b ,cutdur);
+
+
+
+// UCHUNCU TAPSHIRIG
+
+// let b = []
+
+// let a = prompt("Array'in uzunlugunu teyin edin")
+
+// for (let i = 0; i < a ;i++);
+// {
+//     for (let i = 0; i < a; i++) {
+    
+//       let userInput = prompt(i+1 + "-ci Reqemi daxil edin: ")  ;
+//       let userNumber = +userInput;
+//       b.push(userNumber);
+//     }
+// }
+
+
+// DORDUNCU TAPSHIRIG
+
+// let b = []
+
+
+
+
+
+//     for (let i = 0; i < 10; i++) {
+    
+//       let userInput = prompt(i+1 + "-ci Sozu daxil edin: ")  ;
+//       b.push(userInput);
+//     }
+
+
+// let max = b[0].length;
+// let min = b[0].length;
+// for (let i = 0; i < b.length; i++){
+    
+//     if (b[i].length > max) {
+//         max = b[i].length
+//     }
+
+//     if (b[i].length < min) {
+//         min = b[i].length
+//     }
+
+// }
+
+// console.log(max ,min);
